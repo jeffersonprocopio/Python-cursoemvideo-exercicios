6967038f0a3870577345ea1cb31b204d6d63ba5b
jeffersonprocopio <jeffersonestudo@outlook.com>
2022-02-26 00:38:28 -0300

    ex056

---
+++ Python_Exercicios/ex056.py
@@ -0,0 +1,7 @@
+from datetime import date
+for p in range (1, 5):
+    print('----- {}ª PESSOA  -----'.format(p))
+    nome = str(input('Nome: ')).strip().upper()
+    idade = int(input('Idade: '))
+    sex = str(input('Sexo [M/F]: '))
+
