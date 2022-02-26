67db125e018fbde222bbc14ab22039964145dc7e
jeffersonprocopio <jeffersonestudo@outlook.com>
2022-02-26 20:22:15 -0300

    Atualização ex056

--- Python_Exercicios/ex056.py
+++ Python_Exercicios/ex056.py
@@ -1,7 +1,25 @@
-from datetime import date
+somaid = 0
+media  = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
 for p in range (1, 5):
     print('----- {}ª PESSOA  -----'.format(p))
-    nome = str(input('Nome: ')).strip().upper()
+    nome = str(input('Nome: ')).strip()
     idade = int(input('Idade: '))
-    sex = str(input('Sexo [M/F]: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    somaid += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+media = somaid / 4
+print('A média de indade do grupo é de {} anos'.format(media))
+print('O homem mais velho tem {} anos e se chama {}'.format(maioridadehomem, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos'.format(totmulher20))
+
 
