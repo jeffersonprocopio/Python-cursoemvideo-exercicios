1e1dade063f31e1795c7eca21bfd3d74f18cf63e
jeffersonprocopio <jeffersonestudo@outlook.com>
2022-02-28 20:07:43 -0300

    aula14
    aula14

---
+++ Python_teste/aula14.py
@@ -0,0 +1,38 @@
+#Estrutura de repetição while
+
+n = 1
+par = impar = 0
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+
+print('O total de número(s) pare(s) digitados foram {}.'.format(par))
+print(('E o total de número(s) ímpare(s) digitados foram {}'.format(impar)))
+print('Fim')
+
+#exemplo 03:
+#r = 'S'
+#while r == 'S':
+#    n = int(input('Digite um valor: '))
+#    r = str(input('Quer continuar? [S/N] ')).strip().upper()
+#print('Fim')
+
+
+#exemplo 02:
+#n = 1
+#while n != 0:
+#    n = int(input('Digite um valor: '))
+#print('Fim')
+
+
+
+# Exemplo 01:
+#c = 0
+#while c < 10:
+#    c = c + 1
+#    print(c)
+#print('Fim')
